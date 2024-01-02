f0033bc6d0ef63286f90315a75f80e23c4fd95da
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2024-01-02 12:58:26 -0500

    feat(responses): add destroy responses

--- app/services/destroy_record.rb
+++ app/services/destroy_record.rb
@@ -4,11 +4,23 @@
 class DestroyRecord
   def initialize(record)
     @record = record
+    @response = ::Responses::ServiceObjects::Destroy.new(@record)
   end
 
   def call
+    before_process
     @record.destroy
+    after_process
+    @response.destroyed
+    @response
   rescue ActiveRecord::InvalidForeignKey
-    false
+    @response.unprocessabled
+    @response
   end
+
+  private
+
+  def after_process; end
+
+  def before_process; end
 end
---
+++ lib/responses/service_objects/destroy.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Responses
+  module ServiceObjects
+    # Basic Response to Destroy a Record on ServiceObject
+    class Destroy < Responses::ServiceObject
+      attr_accessor :errors
+
+      def initialize(record)
+        super()
+        @record = record
+        @status = StatusCodes::Success.accepted
+        @errors = ActiveModel::Errors.new(@record)
+      end
+
+      def destroyed
+        @status = StatusCodes::Success.ok
+        complete_service
+      end
+
+      def unprocessabled
+        @status = StatusCodes::ClientError.unprocessable_entity
+      end
+    end
+  end
+end
