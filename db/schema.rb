2b122d3e2ebceb59f2412ec2b7a8855e1c319e1e
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2024-01-11 10:07:58 -0500

    fix: comment erb config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_02_202319) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_042816) do
   create_table "deliverables", force: :cascade do |t|
     t.string "name"
     t.datetime "deadline"
@@ -23,6 +23,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_02_202319) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "r_tests", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
