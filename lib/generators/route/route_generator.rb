872d861cef3eb9470420d65d37306915c074566c
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2024-01-11 12:54:58 -0500

    feat(generators): add module routes

--- lib/generators/route/route_generator.rb
+++ lib/generators/route/route_generator.rb
@@ -7,34 +7,39 @@ class RouteGenerator < Rails::Generators::NamedBase
   def gen_init
     return if options[:actions].present?
 
-    @route_class_name = "#{[regular_class_path, file_name.pluralize.camelcase].flatten.map(&:camelcase)
-      .join('::')}Routes"
-
+    create_module_files unless behavior == :revoke
     create_route_file
-
-    # mods = Array(regular_class_path)
-    extended_route = @route_class_name
-    # return if behavior == :revoke && extended_route != @route_class_name
-
-    insert_extend_route('config/routes.rb', extended_route)
   end
 
   private
 
+  def root_routes
+    'config/routes.rb'
+  end
+
   def create_route_file
-    template 'route.template', route_path
+    @route_class_name = route_class_name(file_name.pluralize.camelcase)
+    template 'route.template', class_route_path
+    insert_route_file
     return if behavior == :revoke
 
     route "resources :#{file_name.pluralize}", namespace: regular_class_path
   end
 
-  def route_path
-    dir = ['config', 'routes', regular_class_path].join('/')
-    "#{dir}/#{file_name.pluralize.underscore}_routes.rb"
+  def insert_route_file
+    if Array(regular_class_path)
+      insert_extend_route(route_path(nil), @route_class_name, after: "router.instance_exec do\n", indentation: 6)
+    else
+      insert_extend_route(root_routes, @route_class_name)
+    end
+  end
+
+  def class_route_path
+    route_path(file_name.pluralize.underscore)
   end
 
   # Original: https://github.com/rails/rails/blob/ef04fbb3b256beececfa44c47c4ec93ac6945e59/railties/lib/rails/generators/rails/resource_route/resource_route_generator.rb
-  # Modified: use a method (route_path) instead of 'config/routes.rb'
+  # Modified: use a method (class_route_path) instead of 'config/routes.rb'
   # Make an entry in Rails routing file <tt>config/routes.rb</tt>
   #
   #   route "root 'welcome#index'"
@@ -50,13 +55,44 @@ class RouteGenerator < Rails::Generators::NamedBase
     in_root do
       routing_code = rebase_indentation(routing_code, 6)
 
-      inject_into_file route_path, routing_code, after: "router.instance_exec do\n", verbose: false, force: false
+      inject_into_file class_route_path, routing_code, after: "router.instance_exec do\n", verbose: false, force: false
     end
   end
 
   def insert_extend_route(file_path, class_name, indentation: 2, after: "routes.draw do\n")
+    return unless File.exist?(file_path)
+
     inject_into_file(file_path, after:) do
       rebase_indentation("extend #{class_name}\n", indentation)
     end
+  end
+
+  def create_module_files
+    selected_modules = []
+    Array(regular_class_path).each do |mod|
+      selected_modules << mod
+      create_module_route(selected_modules)
+    end
+  end
+
+  def create_module_route(selected_modules)
+    @route_class_name = route_class_name(nil, mods: selected_modules)
+    template 'route.template', route_path(nil, mods: selected_modules)
+    prev_mods = selected_modules[..-2]
+    prev_path = prev_mods.any? ? route_path(nil, mods: prev_mods) : root_routes
+    after_line = prev_mods.any? ? "router.instance_exec do\n" : "routes.draw do\n"
+    indentation = prev_mods.any? ? 6 : 2
+    insert_extend_route(prev_path, @route_class_name, after: after_line, indentation:)
+  end
+
+  def route_class_name(class_name, mods: regular_class_path)
+    names = [mods, class_name].flatten.compact
+    "#{names.map(&:camelcase).join('::')}Routes"
+  end
+
+  def route_path(class_name, mods: regular_class_path)
+    dirs = %w[config routes]
+    names = [mods, class_name].flatten.compact
+    "#{[dirs, names].flatten.join('/')}_routes.rb"
   end
 end
