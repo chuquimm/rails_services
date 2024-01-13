09d5a8308ab62e7ba2a9aa286937d3e43e9778a2
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2024-01-13 00:00:42 -0500

    feat(wortflow): extract wortflow_scaffold_controller and add parent

--- lib/generators/wortflow/wortflow_generator.rb
+++ lib/generators/wortflow/wortflow_generator.rb
@@ -20,15 +20,11 @@ module Rails
       remove_hook_for :resource_controller
       remove_class_option :actions
 
-      class_option :api, type: :boolean,
-                         desc: 'Generate API-only controller and tests, with no view templates'
-      class_option :resource_route, type: :boolean, default: false
+      # class_option :api, type: :boolean,
+      #                    desc: 'Generate API-only controller and tests, with no view templates'
+      # class_option :resource_route, type: :boolean, default: false
 
-      hook_for :scaffold_controller, required: true
-
-      invoke :servi_craft # generate services
-      invoke :route_tour # generate routes
-      invoke :sobremesa # generate locales
+      invoke :wort_scaffold_controller
     end
   end
 end
